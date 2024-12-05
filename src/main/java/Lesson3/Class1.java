5364f04f8e7a8c03082cd1342afaefbd0def27b5
Tatiana Mandrukevich <tatiana@MacBook-Air-Tatana.local>
2024-12-06 00:07:29 +0300

    second commit

--- src/main/java/Lesson3/Class1.java
+++ src/main/java/Lesson3/Class1.java
@@ -2,6 +2,6 @@ package Lesson3;
 
 public class Class1 {
     public static void main(String[] args) {
-        System.out.println("Первый класс для занятия 3");
+        System.out.println("Изменения в новой ветке");
     }
 }
