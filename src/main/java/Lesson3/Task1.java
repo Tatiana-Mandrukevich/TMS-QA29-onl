bea77d18ba0c41ebda6ad9105117408f9b8abf9a
Tatiana Mandrukevich <tatiana.mandrukevich@icloud.com>
2024-12-10 23:07:33 +0300

    New 2

--- src/main/java/Lesson3/Task1.java
+++ src/main/java/Lesson3/Task1.java
@@ -1,7 +1,7 @@
 package Lesson3;
 
 public class Task1 {
-    public static void run(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Домашняя работа для 3 урока выполнена!");
     }
 }
