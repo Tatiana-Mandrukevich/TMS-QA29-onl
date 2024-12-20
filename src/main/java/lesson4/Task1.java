1fba702b223e5b6db6221bae7ba3a29cee38044d
Tatiana Mandrukevich <tatiana.mandrukevich@icloud.com>
2024-12-20 17:24:45 +0300

    Change names.

--- src/main/java/lesson4/Task1.java
+++ src/main/java/lesson4/Task1.java
@@ -1,4 +1,4 @@
-package Lesson4;
+package lesson4;
 
 public class Task1 {
     public static void main(String[] args) {
--- src/main/java/lesson6/task2/Task2.java
+++ src/main/java/lesson6/task2/Task2.java
@@ -1,6 +1,6 @@
 //Вызвать данный метод необходимо через имя класса, без создания объекта.
 
-package Lesson6.Task2;
+package lesson6.task2;
 
 public class Task2 {
     public static void main(String[] args) {
